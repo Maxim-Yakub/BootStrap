4644f83ad8e0c650949e7fa97bc6d95701fdb67c
Work <79774488441@ya.ru>
2022-08-25 15:58:15 +0300

    nav

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Entity
 @Data
@@ -54,4 +55,9 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public Collection<String> getRolesList() {
+        return roles.stream().map(x->x.getName()).collect(Collectors.toList());
+    }
+
 }
